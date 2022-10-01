d14e56e8b9e9b57112191763c64c68f8d5370acd
Paige <prhoward97@gmail.com>
2022-09-30 20:27:22 -0400

    commit tree a4bf977fdd957e254ec6b93c67602bf3f33c368b

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    const newCats = cats.push(name);
+    return newCats;
+}
+
+function destructivelyPrependCat(name) {
+    const newNewCats = cats.unshift(name);
+    return newNewCats;
+}
+
+function destructivelyRemoveLastCat(name) {
+    const newwwCats = cats.pop(name);
+    return newwwCats;
+} 
+
+function destructivelyRemoveFirstCat(name) {
+    const newNewNewCats = cats.shift(name);
+    return newNewNewCats;
+}
+
+function appendCat(name) {
+    const fuckingCats = [...cats, name];
+    return fuckingCats;
+}
+function prependCat(name) {
+    const moreMotherFuckingCats = [name, ...cats];
+    return moreMotherFuckingCats;
+}
+function removeFirstCat() {
+    const fuckingCatsBro = cats.slice(1);
+    return fuckingCatsBro;
+}
+function removeLastCat() {
+    const wtf = cats.slice(0,-1);
+    return wtf;
+}
